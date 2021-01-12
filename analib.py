db2492ecd05be8b47c6c5a69e214b4245245ec3a
kul0l0 <hanhe0709@gmail.com>
2021-01-12 23:49:46 +0800

    complete to save dictionary in the EDA file.

--- analib.py
+++ analib.py
@@ -35,64 +35,103 @@ def get_files_type_paths(home_path):
     return file_type_path_dict
 
 
-def csv_get_attribute(file_path):
-    """
-    return a DataFrame that contain dtypes of the provided data
-
-    :param file_path: csv data file path
-    :type file_path: str
-    :return: a DataFrame that contain dtypes
-    :rtype: DataFrame
-    """
-    df = pd.read_csv(file_path, nrows=const.NROWS)
-    df = pd.DataFrame(df.dtypes, columns=['type'])
-    df.insert(loc=0, column='name', value = df.index)
-
-    file_name = str.split(file_path, '.')[0].split('/')[-1]
-    df['file_name'] = file_name
-    #df.insert(loc=-1, column='file_name', value = file_name)
-    return df
-
-def generate_dictionary(dir_path, result_path='./', save=True):
+def generate_dictionary(base_df=None, file_path=None, output_path='./', save=True):
     """
     generate a data dictionary.
     save to the result path or return directly.
 
-    :param dir_path: a dir path contain all data file
-    :type dir_path: str
-    :param result_path: the path save dictionary
-    :type result_path: str
+    :param base_df: a DataFrame
+    :type base_df: DataFrame
+    :param file_path: a dir path contain all data file
+    :type file_path: str
+    :param output_path: the path save dictionary
+    :type output_path: str
     :param save: if save=True save a file, else return the dictionary
     :type save: bool
     :return: a Dataframe of dictionary
     :rtype: DataFrame
     """
-    file_type_paths = get_files_type_paths(dir_path)
+    # input check
+    if base_df is None and file_path is None:
+        print('Please, provide a df or a path of data')
+        raise
 
-    dictionary = pd.DataFrame()
-    options = {
-        'csv': csv_get_attribute,
+    # working
+    get_df = {
+        'csv': pd.read_csv,
     }
-    for file_type, file_paths in file_type_paths.items():
-        if file_type not in const.DATA_FILE_TYPE:
-            continue
-        for file_path in file_paths:
-            df = options[file_type](file_path)
-            dictionary = pd.concat([dictionary, df])
+    if base_df is None:
+        file_type = file_path.split('.')[-1]
+        base_df = get_df[file_type](file_path)
 
-    dictionary.drop_duplicates(ignore_index=True, inplace=True)
+    dictionary = pd.DataFrame(base_df.dtypes, columns=['Type'])
+    dictionary.insert(loc=0, column='Name', value = dictionary.index)
+    dictionary['Explanation'] = None
 
+    # output check
     if save:
-        dictionary.to_csv(os.path.join(result_path, 'dictionary.csv'), index=False)
+        if file_path:
+            output_name = file_path.split('.')[0].split('/')[-1]
+        else:
+            output_name = 'temp'
+        dictionary.to_csv(os.path.join(output_path, '%s_dict.csv'%output_name), index=False)
     else:
         return dictionary
 
+def write_excel(file_path, extra='', sheet_names=[], sheet_contents=[]):
+    with pd.ExcelWriter("%s%s.xlsx"%(file_path, extra)) as ew:
+        for sheet_name, df in zip(sheet_names, sheet_contents):
+            df.to_excel(excel_writer=ew, sheet_name=sheet_name, index=False)
 
 class EDA(object):
-    def __init__(self, dir_path=None):
 
-        # working
-        type_paths = get_files_type_paths(dir_path)
+    def __init__(self, data_dir_path=None, output_path=None):
+        self.data_dir_path = data_dir_path
+        self.output_path = output_path
+        self.get_df = {
+            'csv': pd.read_csv,
+        }
+        self.df_list = []
+        self.sheet_names = []
+        self.sheet_contents = []
+        # self.base_EDA()
+
+
+    def base_EDA(self):
+        type_paths = get_files_type_paths(self.data_dir_path)
+        for file_type, file_paths in type_paths.items():
+            if file_type not in const.DATA_FILE_TYPE:
+                continue
+            for file_path in file_paths:
+                df = self.get_df[file_type](file_path)
+                self.df_list.append(df)
+                file_name = file_path.split('.')[0].split('/')[-1]
+                # dictionary
+                dict_df = generate_dictionary(base_df=df, save=False)
+                self.sheet_names.append("Dictionary")
+                self.sheet_contents.append(dict_df)
+                # write excel
+                write_excel(
+                    file_path=os.path.join(self.output_path, file_name),
+                    extra='_EDA',
+                    sheet_names=self.sheet_names,
+                    sheet_contents=self.sheet_contents
+                )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
